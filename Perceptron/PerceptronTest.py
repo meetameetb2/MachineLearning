3a49a6cbcf0a0c46647d29341c5b92dd25049d12
meetameetb2 <meetameetb2@gmail.com>
2020-02-17 20:56:52 +0530

    commit portion

--- Perceptron/PerceptronTest.py
+++ Perceptron/PerceptronTest.py
@@ -11,9 +11,9 @@ y = np.where(y == 'Iris-setosa', -1,1) # if y == 'Iris-setosa', make class label
 
 X = df.iloc[0:100,[0,2]].values # petal length and sepal length are selected
 
-plt.scatter(X[:50, 0], X[:50, 1],color = 'red', marker = 'o', label = 'setosa')
-plt.scatter(X[50:100, 0], X[50:100, 1], color = 'blue', marker = 'x', label = 'versicolor')
-plt.xlabel('sepal length [cm]')
-plt.ylabel('petal length [cm]')
-plt.legend(loc='upper left')
-plt.show()
+# plt.scatter(X[:50, 0], X[:50, 1],color = 'red', marker = 'o', label = 'setosa')
+# plt.scatter(X[50:100, 0], X[50:100, 1], color = 'blue', marker = 'x', label = 'versicolor')
+# plt.xlabel('sepal length [cm]')
+# plt.ylabel('petal length [cm]')
+# plt.legend(loc='upper left')
+# plt.show()
