1a5db8d9f47c993855f18833e590ccdcd023bc0e
meetameetb2 <meetameetb2@gmail.com>
2020-02-21 15:41:38 +0530

    correcting issue

---
+++ Adaline/AdalineTest.py
@@ -0,0 +1,30 @@
+import matplotlib.pyplot as plt
+import pandas as pd
+import numpy as np
+from Adaline.AdalineGD import AdalineGD
+
+df = pd.read_csv("iris.data",header=None)
+print(df.tail())
+# select setosa and versicolor
+y = df.iloc[0:100, 4].values
+y = np.where(y == 'Iris-setosa', -1, 1)
+
+# extract sepal length and petal length
+X = df.iloc[0:100, [0, 2]].values
+
+fig, ax = plt.subplots(nrows=1, ncols=2, figsize=(10, 4))
+
+ada = AdalineGD(n_iter=10, eta=0.01)
+ada1 = ada.fit(X,y)
+# ada1 = AdalineGD(n_iter=10, eta=0.01).fit(X, y)
+ax[0].plot(range(1, len(ada1.cost_) + 1), np.log10(ada1.cost_), marker='o')
+ax[0].set_xlabel('Epochs')
+ax[0].set_ylabel('log(Sum-squared-error)')
+ax[0].set_title('Adaline - Learning rate 0.01')
+
+ada2 = AdalineGD(n_iter=10, eta=0.0001).fit(X, y)
+ax[1].plot(range(1, len(ada2.cost_) + 1), ada2.cost_, marker='o')
+ax[1].set_xlabel('Epochs')
+ax[1].set_ylabel('Sum-squared-error')
+ax[1].set_title('Adaline - Learning rate 0.0001')
+plt.show()
